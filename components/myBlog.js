64da2bcfd7b80869d6587cc235e99c8f7bab3b1d
realrufans <realboyswag9@gmail.com>
2022-12-21 19:31:55 +0100

    added unique key to blogs posts

--- components/myBlog.js
+++ components/myBlog.js
@@ -107,56 +107,56 @@ function Myblog() {
             return (
               <SwiperSlide
                 className="flex justify-between items-center gap-3 mb-10   "
-
+                key={i}
               >  <CustomNavigation prev={true} />
 
-                <div key={i}>
 
 
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={`https://hive.blog/${post.url.substring(5, [
-                      post.url.length,
-                    ])}`}
-                  >
-                    {" "}
-                    <div className="hover:scale-110 hover:bg-black-900    border-b-2 border-blue/10 my-10    max-w-lg   cursor-pointer  shadow-black/20 shadow-xl mx-auto mt-0 bg-red-50/20">
-                      <Image
-                        objectFit="cover"
-                        layout="responsive"
-                        width={150}
-                        height={100}
-                        src={json.image[0]}
-                        alt="rufans on hive"
+
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={`https://hive.blog/${post.url.substring(5, [
+                    post.url.length,
+                  ])}`}
+                >
+                  {" "}
+                  <div className="hover:scale-110 hover:bg-black-900    border-b-2 border-blue/10 my-10    max-w-lg   cursor-pointer  shadow-black/20 shadow-xl mx-auto mt-0 bg-red-50/20">
+                    <Image
+                      objectFit="cover"
+                      layout="responsive"
+                      width={150}
+                      height={100}
+                      src={json.image[0]}
+                      alt="rufans on hive"
 
 
-                      />
+                    />
 
-                      <div className="px-6 py-4">
-                        <h2 className="font-extrabold text-white-900 mb-2">{post.title.substring(0, 150)}</h2>
-                        <p className=" text-sm">{postBody}</p>
-                      </div>
-                      <div className="px-6   ">
-                        {json.tags.map((tag, i) => {
-                          return (
-                            <span
-                              key={i}
-                              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2"
-                            >
-                              {tag}
-                            </span>
-                          );
-                        })}
-                      </div>
-                      <div className="px-6 py-4">
-                        <span className="inline-block bg-gray-200  rounded-full px-3 py-1 text-xs font-semibold text-green-900 mr-2">
-                          {postDate}
-                        </span>
-                      </div>
+                    <div className="px-6 py-4">
+                      <h2 className="font-extrabold text-white-900 mb-2">{post.title.substring(0, 150)}</h2>
+                      <p className=" text-sm">{postBody}</p>
                     </div>
-                  </a>
-                </div>
+                    <div className="px-6   ">
+                      {json.tags.map((tag, i) => {
+                        return (
+                          <span
+                            key={i}
+                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2"
+                          >
+                            {tag}
+                          </span>
+                        );
+                      })}
+                    </div>
+                    <div className="px-6 py-4">
+                      <span className="inline-block bg-gray-200  rounded-full px-3 py-1 text-xs font-semibold text-green-900 mr-2">
+                        {postDate}
+                      </span>
+                    </div>
+                  </div>
+                </a>
+
                 <CustomNavigation next={true} />
               </SwiperSlide>);
 
