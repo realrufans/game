69098bbe182b1baf7045e8b0d33708e3decdd778
solomon stephen <33923021+realrufans@users.noreply.github.com>
2023-11-27 15:56:24 +0100

    added e-create

--- utils/projectsData.js
+++ utils/projectsData.js
@@ -1,13 +1,21 @@
 export const projects = [
-   {
+  // {
+  //   name: "eCrea8",
+  //   image: "/images/e-creat8.png",
+  //   description:
+  //     "We transform your ideas into alluring digital realities and unleash boundless innovation. With a wide range of services, ranging from cybersecurity, UI/UX design, Front-end and Backend development, Project and Product management, we have all your digital aspirations covered.",
+  //   tags: "#tailwindcss #nextjs",
+  //   url: "https://ecrea8-website.vercel.app/",
+  // },
+  {
     image: "/images/pdfcounter.png",
     name: "PDF Keyword Counter",
     description:
       "Determine the number of times your keywords appear in a PDF document. Upload your PDF, enter keywords, and get keyword count results.",
-    tags: "#nodejs #nextj #javascript",
+    tags: "#nodejs #nextjs #javascript",
     url: "https://pdf-keyword-counter-fontend.vercel.app",
   },
-  
+
   {
     name: "Coinsino Finance",
     image: "/images/coinsino.png",
@@ -25,7 +33,14 @@ export const projects = [
   //   url: "https://telostask.app/",
   // },
   {
-  
+    name: "eCrea8",
+    image: "/images/e-creat8.png",
+    description:
+      "We transform your ideas into alluring digital realities and unleash boundless innovation. With a wide range of services, ranging from cybersecurity, UI/UX design, Front-end and Backend development, Project and Product management, we have all your digital aspirations covered.",
+    tags: "#tailwindcss #nextjs",
+    url: "https://ecrea8-website.vercel.app/",
+  },
+  {
     name: "Discord-clone",
     image: "/images/discordclone.png",
     description: `Imagine a place where you can belong to a school club, a
@@ -34,23 +49,20 @@ export const projects = [
     tags: "#reactjs #tailwindcss #firebase",
     url: "https://realrufans.github.io/discord-clone/",
   },
-  {
-
-    name: "Mydappr",
-    image: "/images/mydappr.png",
-    alt: "mydappr by rufans",
-    description: ` MydAppr is a decentralized community incubator that aims to ideate, develop, launch and market different dApp projects through its Community dApp Ideation (CDI) platform.`,
-    tags: "#html #css #lottie",
-    url: "https://mydappr.io/",
-  },
-  {
- 
-    name: "Pac-Man",
-    image: "/images/pacman.png",
-    alt: "Pac-Man by rufans",
-    description: `This is a clone of the Pac-Man game. I developed this game while I was just starting my front-end development journey. Built it with vanilla js,HTML and CSS. `,
-    tags: "#html #css #javascript",
-    url: "https://realrufans.github.io/pacman/",
-  },
+  // {
+  //   name: "Mydappr",
+  //   image: "/images/mydappr.png",
+  //   alt: "mydappr by rufans",
+  //   description: ` MydAppr is a decentralized community incubator that aims to ideate, develop, launch and market different dApp projects through its Community dApp Ideation (CDI) platform.`,
+  //   tags: "#html #css #lottie",
+  //   url: "https://mydappr.io/",
+  // },
+  // {
+  //   name: "Pac-Man",
+  //   image: "/images/pacman.png",
+  //   alt: "Pac-Man by rufans",
+  //   description: `This is a clone of the Pac-Man game. I developed this game while I was just starting my front-end development journey. Built it with vanilla js,HTML and CSS. `,
+  //   tags: "#html #css #javascript",
+  //   url: "https://realrufans.github.io/pacman/",
+  // },
 ];
-
