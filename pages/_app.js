80c9cf1346732c760323bd9f1bedcae6009f4e80
solomon stephen <33923021+realrufans@users.noreply.github.com>
2024-10-01 21:13:00 +0100

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+ 
       <ThemeProvider attribute="class" enableSystem={true}>
         <Header />
         <Component {...pageProps} />
